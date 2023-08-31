ada86d77e37f682f6aa58e67d87dd4960e3d1062
Fran Nicolas <fjnicolas@ugr.es>
2023-08-31 18:35:49 +0200

    remove unnecesary files

--- main.py
+++
@@ -1,5 +0,0 @@
-import mygeek
-
-geek = pybind_wrapper.MyGeek(2)
-
-print("Result from C++ function:", geek.HelloGeek())
--- src/mygeek.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "mygeek.h"
-
-MyGeek::MyGeek(int val) : value(val) {}
-
-void MyGeek::HelloGeek() {
-    std::cout << "Hello geek! The value is: " << value << std::endl;
-}
